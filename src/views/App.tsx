8066c3b2f8582db7fff6b36f70c8803bfe483af3
@wojtek-yordex <wojtek@yordex.com>
2022-06-02 15:16:16 +0100

    Add focus view on log in

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,4 +1,4 @@
-import { Banner, Box, Button, ContextView, TextField } from "@stripe/ui-extension-sdk/ui";
+import { Banner, Box, Button, ContextView, FocusView, Img, TextField, } from "@stripe/ui-extension-sdk/ui";
 import type { ExtensionContextValue } from "@stripe/ui-extension-sdk/context";
 import BrandIcon from "./brand_icon.svg";
 import { useState, useCallback } from "react";
@@ -7,12 +7,16 @@ const App = ({ userContext, environment }: ExtensionContextValue) => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [session, setSession] = useState<any | undefined>(undefined)
+  const [sessionPanelVisible, setSessionPanelVisible] = useState(false)
   const [error, setError] = useState<string>('')
+
   const onLogIn = useCallback(() => {
+    setError('')
     fetch('https://new-app.dev.yordex.com/api/sessions', { method: 'POST', headers: { 'content-type': 'application/json' }, body: JSON.stringify({ username, password }), })
       .then(res => res.json())
       .then(res => {
         setSession(res)
+        setSessionPanelVisible(true)
       }).catch(e => {
         setError(e.message)
       })
@@ -35,7 +39,12 @@ const App = ({ userContext, environment }: ExtensionContextValue) => {
         <Box css={{ marginTop: 'small' }}>
           <Button onPress={onLogIn}>Log in</Button>
         </Box>
-        {session && <Banner title={'Welcome'} description={`You are logged in to yordex as: ${session.firstName} ${session.lastName}`} />}
+        <FocusView shown={sessionPanelVisible} title="Welcome" onClose={() => { setSessionPanelVisible(false) }}>
+          {session && <Box css={{ height: 'auto', stack: 'y', alignY: 'top' }}>
+            You are logged in to yordex as: {session.firstName} {session.lastName}
+            <Img src="https://blocventures.com/wp-content/uploads/2020/03/yordex_1.png" />
+          </Box>}
+        </FocusView>
       </Box>
     </ContextView >
   );
